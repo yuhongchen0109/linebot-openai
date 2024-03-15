e033b601308d5962bd95d458e2540ab62b4200a9
yuhongchen0109 <163223187+yuhongchen0109@users.noreply.github.com>
2024-03-15 17:34:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-from opencc import OpenCC
-cc = OpenCC('s2t')  # 建立一個簡體中文轉繁體中文的轉換器
-
 from flask import request, abort
 from linebot import  LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
@@ -28,10 +25,10 @@ def callback():
 @handler1.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text1=event.message.text
-    text1 = cc.convert(text1)  # 將用戶的輸入轉換為繁體中文
     response = openai.ChatCompletion.create(
         messages=[
-            {"role": "system", "content": "You are not only a knowledgeable history teacher but also novelist.you come from germany.you know most of thing about world war 2."},
+            {"role": "system", "content": "You are not only a knowledgeable history teacher but also novelist.you come from germany.you know most of thing about world war 2.
+            convert the output results into Traditional Chinese."},
             {"role": "user", "content": text1}
         ],
         model="gpt-3.5-turbo-0125",
@@ -39,7 +36,6 @@ def handle_message(event):
     )
     try:
         ret = response['choices'][0]['message']['content'].strip()
-        ret = cc.convert(ret)  # 將回應轉換為繁體中文
     except:
         ret = '發生錯誤！'
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=ret))
