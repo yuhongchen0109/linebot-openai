8f94fe91554320f0da716553ec92a3195c1498ed
yuhongchen0109 <163223187+yuhongchen0109@users.noreply.github.com>
2024-03-15 16:02:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,35 +13,30 @@ line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 handler1 = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
 @app.route('/callback', methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    try:
-        handler1.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-
-@handler1.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    user_message = f"""我想學習英文~"""
-    text1=event.message.text
+    text1 = event.message.text
+
+    messages =  [
+        {
+            'role':'system',
+            'content': "你是一個專業的外語老師，請以簡短的方式對話"
+        },
+        { # 這裏存放的是使用者增進輸入的訊息
+            'role':'user',
+            'content': "你好，很高興認識你"
+        },
+        { # 這個則是聊天機器人的回應訊息
+            'role':'assistant',
+            'content': f"你好！我也很高興認識你。有什麼外語方面的問題我可以幫助你解答嗎？"
+        },
+        { # 這個則是這次使用者輸入的訊息
+            'role':'user',
+            'content': f"{text1}"
+        }
+    ]
+
     response = openai.ChatCompletion.create(
-      messages =  [
-   
-    { # 這裏存放的是使用者增進輸入的訊息
-        'role':'user',
-        'content': "你好，很高興認識你"
-    },
-    { # 這個則是聊天機器人的回應訊息
-        'role':'assistant',
-        'content': f"你好！我也很高興認識你。有什麼外語方面的問題我可以幫助你解答嗎？"
-    },
-    { # 這個則是這次使用者輸入的訊息
-        'role':'user',
-        'content': f"{user_message}"
-    }
-],
+        messages=messages,
         model="gpt-3.5-turbo-0125",
         temperature = 0.5,
     )
