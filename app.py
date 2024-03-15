7c540b53536983082756364434473ce90f8a3c16
yuhongchen0109 <163223187+yuhongchen0109@users.noreply.github.com>
2024-03-15 15:24:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,10 +26,24 @@ def callback():
 def handle_message(event):
     text1=event.message.text
     response = openai.ChatCompletion.create(
-        messages=[
-            {
-        'role':'system','content': "你是一個專業的外語老師，請以簡短的方式對話。"}
-        ],
+      messages =  [
+    {
+        'role':'system',
+        'content': "你是一個專業的外語老師，請以簡短的方式對話"
+    },
+    { # 這裏存放的是使用者增進輸入的訊息
+        'role':'user',
+        'content': "你好，很高興認識你"
+    },
+    { # 這個則是聊天機器人的回應訊息
+        'role':'assistant',
+        'content': f"你好！我也很高興認識你。有什麼外語方面的問題我可以幫助你解答嗎？"
+    },
+    { # 這個則是這次使用者輸入的訊息
+        'role':'user',
+        'content': f"{user_message}"
+    }
+],
         model="gpt-3.5-turbo-0125",
         temperature = 0.5,
     )
