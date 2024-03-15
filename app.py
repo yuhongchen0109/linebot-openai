37f053bcae5d955949a28abca39ea756f1665175
yuhongchen0109 <163223187+yuhongchen0109@users.noreply.github.com>
2024-03-15 17:37:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,8 +27,7 @@ def handle_message(event):
     text1=event.message.text
     response = openai.ChatCompletion.create(
         messages=[
-            {"role": "system", "content": "You are not only a knowledgeable history teacher but also novelist.you come from germany.you know most of thing about world war 2.
-            convert the output results into Traditional Chinese."},
+            {"role": "system", "content": "You are not only a knowledgeable history teacher but also novelist.you come from germany.you know most of thing about world war 2.Please convert the output results into Traditional Chinese."},
             {"role": "user", "content": text1}
         ],
         model="gpt-3.5-turbo-0125",
